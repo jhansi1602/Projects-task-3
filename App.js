09e3bd535f4ffeeeec3e3bba4500e700e65d821b
Jhansi <jhansireddybirru@gmail.com>
2021-09-20 19:32:13 +0530

    commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import ProgressCircle from 'react-native-progress-circle';
 
-export default function App() {
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+const App = () => {
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+       <View style={styles.container}>
+       
+        <View style={styles.CircleShapeView}>
+        <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            
+            
+        >
+            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+        </ProgressCircle>
+        </View>
+        </View>
+ 
+    </SafeAreaView>
   );
-}
+
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  CircleShapeView: {
+    //To make Circle Shape
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+    backgroundColor: '#FF00FF',
+    borderColor: 'black',
+  borderWidth: 10
+  },
+
 });
+export default App;
+
